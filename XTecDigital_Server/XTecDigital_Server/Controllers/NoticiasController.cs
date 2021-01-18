8e41ca4052e448d2866a8c3156b651a57928613a
Fabian Ramirez <ramirezfabian003@gmail.com>
2021-01-17 21:15:04 -0600

    Se agregan noticias por grupo

---
+++ XTecDigital_Server/XTecDigital_Server/Controllers/NoticiasController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using XTecDigital_Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace XTecDigital_Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NoticiasController : ControllerBase   
+    {
+        private string serverKey = Startup.getKey();
+
+        [Route("crearNoticiaGrupo")]
+        [EnableCors("AnotherPolicy")]
+        [HttpPost]
+        public Object CrearNoticiaGrupo(Noticia noticia)
+        {
+            SqlConnection conn = new SqlConnection(serverKey);
+            conn.Open();
+            string insertQuery = "crearNoticiaGrupo";
+            SqlCommand cmd = new SqlCommand(insertQuery, conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@codigoCurso", noticia.codigoCurso);
+            cmd.Parameters.AddWithValue("@numeroGrupo", noticia.numeroGrupo);
+            cmd.Parameters.AddWithValue("@tituloNoticia", noticia.tituloNoticia);
+            cmd.Parameters.AddWithValue("@mensaje", noticia.mensaje);
+            List<Object> respuesta = new List<Object>();
+            try
+            {
+                cmd.ExecuteNonQuery();
+                var response = new[]
+                    {
+                        new
+                        {
+                            respuesta = "200 OK",
+                            error = "null"
+                        }
+
+                     };
+                respuesta.Add(response);
+            }
+            catch (Exception e)
+            {
+                string[] separatingStrings = { "\r" };
+                var response = new[]
+                    {
+                        new
+                        {
+                            respuesta = "error",
+                            error = e.Message.Split(separatingStrings, System.StringSplitOptions.RemoveEmptyEntries)[0]
+            }
+
+                     };
+                respuesta.Add(response);
+            }
+            conn.Close();
+            return respuesta[0];
+        }
+    }
+}
---
+++ XTecDigital_Server/XTecDigital_Server/Models/Noticia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XTecDigital_Server.Models
+{
+    public class Noticia
+    {
+        public Noticia()
+        {
+        }
+        public string codigoCurso { get; set; }
+        public int numeroGrupo { get; set; }
+        public string tituloNoticia { get; set; }
+        public string mensaje { get; set; }
+    }
+}
